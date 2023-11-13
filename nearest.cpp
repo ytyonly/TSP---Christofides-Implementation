7a37e4a5163f78db02dd3ba38d36fca1ec00fff5
Tian0602 <646432316@qq.com>
2023-11-13 01:05:29 +0100

    nearest 0.1ver

--- nearest.cpp
+++ nearest.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+
 using namespace std;
 
 struct Point {
@@ -24,7 +28,6 @@ vector<int> nearestNeighbor(const vector<Point>& points, vector<vector<int>> dis
     int current = 0;
     tour.push_back(current);
     visited[current] = true;
-
 
     // Build the tour
     for (int i = 1; i < N; ++i) {
@@ -76,9 +79,53 @@ void perform2Opt(vector<int>& tour, const vector<Point>& points, vector<vector<i
     }
 }
 
+// Function to compute the delta for a 3-opt move
+int compute3OptDelta(const vector<int>& tour, const vector<vector<int>>& dis, size_t i, size_t j, size_t k) {
+    int oldCost = dis[tour[i]][tour[i + 1]] + dis[tour[j]][tour[j + 1]] + dis[tour[k]][tour[(k + 1) % tour.size()]];
+
+    int newCost = dis[tour[i]][tour[k]] + dis[tour[j]][tour[i + 1]] + dis[tour[k]][tour[(j + 1) % tour.size()]];
+
+    return newCost - oldCost;
+}
+
+// Function to apply a 3-opt move
+void apply3OptMove(vector<int>& tour, size_t i, size_t j, size_t k) {
+    reverseSection(tour, i + 1, j);
+    reverseSection(tour, j + 1, k);
+}
+
+void perform3Opt(vector<int>& tour, const vector<Point>& points, vector<vector<int>> dis) {
+    int trynum = 10000;
+    while (trynum--) {
+        // improvement = false;
+        int bestDelta = 0;
+        size_t bestI = 0, bestJ = 0, bestK = 0;
+        for (size_t i = 0; i < tour.size() - 2; ++i) {
+            for (size_t j = i + 1; j < tour.size() - 1; ++j) {
+                for (size_t k = j + 1; k < tour.size(); ++k) {
+                    int delta = compute3OptDelta(tour, dis, i, j, k);
+
+                    if (delta < bestDelta) {
+                        bestDelta = delta;
+                        bestI = i;
+                        bestJ = j;
+                        bestK = k;
+                    }
+                }
+            }
+        }
+        if (bestDelta < 0) {
+            apply3OptMove(tour, bestI, bestJ, bestK);
+            // improvement = true;
+        }
+    }
+}
+
 int main() {
+
     int N;
     cin >> N;
+
     vector<Point> points;
     points.reserve(N);
 
@@ -88,22 +135,32 @@ int main() {
         points.emplace_back(x, y);
     }
 
+    if(N == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+
     vector<vector<int>> dis(N, vector<int>(N, 0));
-    for(int i = 0; i < N; ++i) {
-        for(int j = 0; j < N; ++j) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
             dis[i][j] = distance(points[i], points[j]);
         }
     }
-    
 
+    // Initialize with nearest neighbor and perform 2-opt optimization
     vector<int> tour = nearestNeighbor(points, dis);
     perform2Opt(tour, points, dis);
-    int tourlen = 0;
-    for(int i = 1; i < tour.size(); ++i) {
-        tourlen += dis[tour[i-1]][tour[i]];
+
+    if(N > 960) {
+        for (int index : tour) {
+            cout << index << endl;
+        }
+        return 0;
     }
-    tourlen += dis[tour[tour.size() - 1]][tour[0]];
-    cout<<"tour length is "<<tourlen<<endl;
+        
+    perform3Opt(tour, points, dis);
+
+    // Output the final tour
     for (int index : tour) {
         cout << index << endl;
     }
